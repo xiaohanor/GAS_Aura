808f019ce057fc94aab4c198a7d876440e59d416
Zihan Li <xiaohanor@qq.com>
2025-04-15 16:10:26 +0800

    添加玩家控制器，设置基础动画蓝图

---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
